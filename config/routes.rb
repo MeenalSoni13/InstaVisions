cc606bc01f7b93c26f558747dd7076335bc25d13
MeenalSoni13 <meenalsoni13@gmail.com>
2023-12-20 17:31:33 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  
+end
